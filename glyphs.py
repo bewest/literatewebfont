15aea603866fc546d7a69f6f9867a13af86b5894
Ben West <bewest@gmail.com>
2012-07-30 02:26:17 -0700

    Easily strip glyphs out of fonts.

--- glyphs.py
+++ glyphs.py
@@ -1,23 +1,63 @@
 #!/usr/bin/python
 
+"""
+Export glyphs from a font file into a theme directory with each glyph
+saved as its own svg file.
+"""
 
 import fontforge
-import sys
+import sys, os, optparse
 
-def inspect_glyphs(font):
-  for glyph in font.glyphs("encoding"):
-    print glyph
+from os import path
+
+from pprint import pprint, pformat
+
+def get_parser( ):
+  parser = optparse.OptionParser( )
+  parser.add_option('-p', '--prefix',
+                    default="themes",
+                    help="Write glyphs to this directory.")
+  parser.add_option('-s', '--suffix',
+                    default="glyph.svg",
+                    help="Use this suffix when writing glyphs.")
+  return parser
 
 
-def inspect_font_file(name):
-  font = fontforge.open(name)
-  inspect_glyphs(font)
+class Inspector(object):
+  def __init__(self, options, args):
+    self.options = options
+    self.args = args
 
+  def run(self):
+    for font in self.args:
+      self.inspect(font)
 
-if '__main__' in [ __name__ ]:
-  for name in sys.argv[1:]:
-    print name
-    inspect_font_file(name)
+  def inspect(self, name):
+    self.font = font = fontforge.open(name)
+    for glyph in font.glyphs('encoding'):
+      if glyph.unicode > 0:
+        self.rip_glyph(glyph)
+
+  def rip_glyph(self, glyph):
+    prefix = self.options.prefix
+    suffix = self.options.suffix
+    code = "%04x" % glyph.unicode
+    short= glyph.glyphname
+    name = '.'.join([code, short, suffix])
+    output = path.join(prefix, name)
+    print output, ' ', glyph.glyphname, ' ', 'code', "%04x" % glyph.unicode
+    glyph.export(output)
+
+def main(*args):
+  parser = get_parser( )
+  (options, args) = parser.parse_args(list(args))
+  prog, args = args[0], args[1:]
+
+  app = Inspector(options, args)
+  app.run( )
+
+if '__main__' == __name__:
+  main(*sys.argv)
 
 #####
 # EOF
