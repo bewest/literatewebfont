94f5ea467fb5a4d4387b499a4042ffd9c6198597
Ben West <bewest@gmail.com>
2012-08-02 00:15:36 -0700

    actually create eot files.

--- glyphs2webfont.sh
+++ glyphs2webfont.sh
@@ -2,10 +2,9 @@
 
 python simple.py $*
 
-
 for theme in $* ; do
   if [[ -d $theme ]] ; then
-    echo ttf2eot "fonts/$theme" > "fonts/$theme.eot"
+    ttf2eot "fonts/$theme.ttf" > "fonts/$theme.eot"
     name=$(basename $theme)
     ./glyphs.py -n fonts/glyphs.svg \
       | cut -d' ' -f 2,4 | sed -e "s|^|icon-|g" \
