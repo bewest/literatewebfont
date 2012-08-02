792b7e6b77079ec989c1e373c032d8409e286331
Ben West <bewest@gmail.com>
2012-08-02 00:55:24 -0700

    tweak comment

--- glyphs2webfont.sh
+++ glyphs2webfont.sh
@@ -6,6 +6,10 @@ for theme in $* ; do
   if [[ -d $theme ]] ; then
     ttf2eot "fonts/$theme.ttf" > "fonts/$theme.eot"
     name=$(basename $theme)
+    # potentially smarter version of this would use xpath+sass
+    # function to generate a list of these to iterate over.  in any
+    # case, list all the glyphs, and generate a css stanza that
+    # generates content using that glyph.
     ./glyphs.py -n fonts/glyphs.svg \
       | cut -d' ' -f 2,4 | sed -e "s|^|icon-|g" \
       | ./glyph2css.sh  > css/$name.css
